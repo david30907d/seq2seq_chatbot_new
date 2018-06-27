02d677f94ca50d9e3a292b566d51b65e1f4ee142
david <davidtnfsh@gmail.com>
2018-06-27 16:23:34 +0800

    download sogou data

--- sogou.sh
+++ sogou.sh
@@ -1,3 +1,5 @@
+wget http://download.labs.sogou.com/dl/sogoulabdown/SogouCA/news_tensite_xml.smarty.tar.gz
+tar zxvf news_tensite_xml.smarty.tar.gz
 cat ./news_tensite_xml.smarty.dat | iconv -f gbk -t utf-8 -c | grep "<content>" | opencc > content.txt
 cat ./news_tensite_xml.smarty.dat | iconv -f gbk -t utf-8 -c | grep "<contenttitle>" | opencc > contenttitle.txt
 cat ./news_tensite_xml.smarty.dat | iconv -f gbk -t utf-8 -c | grep "<url>" | opencc > url.txt
