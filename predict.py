b59ff26afe34e48c11656dfd8956e63804e7f2c5
張泰瑋 <zhangtaiwei@zhangtaiweide-MacBook-Pro.local>
2018-07-03 11:38:44 +0800

    esourceExhaustedError (see above for traceback): OOM when allocating tensor with shape[785503,200] and type float on /job:localhost/replica:0/task:0/device:GPU:0 by allocator GPU_0_bfc

--- predict.py
+++ predict.py
@@ -5,33 +5,21 @@ import sys
 import numpy as np
 
 
-tf.app.flags.DEFINE_integer('rnn_size', 1024, 'Number of hidden units in each layer')
+tf.app.flags.DEFINE_integer('rnn_size', 50, 'Number of hidden units in each layer')
 tf.app.flags.DEFINE_integer('num_layers', 2, 'Number of layers in each encoder and decoder')
-tf.app.flags.DEFINE_integer('embedding_size', 1024, 'Embedding dimensions of encoder and decoder inputs')
+tf.app.flags.DEFINE_integer('embedding_size', 200, 'Embedding dimensions of encoder and decoder inputs')
 
 tf.app.flags.DEFINE_float('learning_rate', 0.0001, 'Learning rate')
-tf.app.flags.DEFINE_integer('batch_size', 128, 'Batch size')
+tf.app.flags.DEFINE_integer('batch_size', 1, 'Batch size')
 tf.app.flags.DEFINE_integer('numEpochs', 30, 'Maximum # of training epochs')
 tf.app.flags.DEFINE_integer('steps_per_checkpoint', 100, 'Save model checkpoint every this iteration')
 tf.app.flags.DEFINE_string('model_dir', 'model/', 'Path to save model checkpoints')
 tf.app.flags.DEFINE_string('model_name', 'chatbot.ckpt', 'File name used for model checkpoints')
+tf.app.flags.DEFINE_string('data_path', 'data/gensim2file.pkl', 'Filepath of intput data')
+
 FLAGS = tf.app.flags.FLAGS
 
-data_path = 'E:\PycharmProjects\seq2seq_chatbot\seq2seq_chatbot_new\data\dataset-cornell-length10-filter1-vocabSize40000.pkl'
-word2id, id2word, trainingSamples = loadDataset(data_path)
-
-def predict_ids_to_seq(predict_ids, id2word, beam_szie):
-    '''
-    将beam_search返回的结果转化为字符串
-    :param predict_ids: 列表，长度为batch_size，每个元素都是decode_len*beam_size的数组
-    :param id2word: vocab字典
-    :return:
-    '''
-    for single_predict in predict_ids:
-        for i in range(beam_szie):
-            predict_list = np.ndarray.tolist(single_predict[:, :, i])
-            predict_seq = [id2word[idx] for idx in predict_list[0]]
-            print(" ".join(predict_seq))
+word2id, id2word, trainingSamples = loadDataset(FLAGS.data_path)
 
 with tf.Session() as sess:
     model = Seq2SeqModel(FLAGS.rnn_size, FLAGS.num_layers, FLAGS.embedding_size, FLAGS.learning_rate, word2id,
@@ -46,10 +34,8 @@ with tf.Session() as sess:
     sys.stdout.flush()
     sentence = sys.stdin.readline()
     while sentence:
-        batch = sentence2enco(sentence, word2id)
-        predicted_ids = model.infer(sess, batch)
-        # print(predicted_ids)
-        predict_ids_to_seq(predicted_ids, id2word, 5)
+        print(model.infer(sess, sentence, isBatch=False))
         print("> ", "")
         sys.stdout.flush()
         sentence = sys.stdin.readline()
+
--- train.py
+++ train.py
@@ -2,33 +2,36 @@ import tensorflow as tf
 from data_helpers import loadDataset, getBatches, sentence2enco
 from model import Seq2SeqModel
 from tqdm import tqdm
-import math
-import os
+import math, os
+import numpy as np
 
-tf.app.flags.DEFINE_integer('rnn_size', 1024, 'Number of hidden units in each layer')
+tf.app.flags.DEFINE_integer('rnn_size', 50, 'Number of hidden units in each layer')
 tf.app.flags.DEFINE_integer('num_layers', 2, 'Number of layers in each encoder and decoder')
-tf.app.flags.DEFINE_integer('embedding_size', 1024, 'Embedding dimensions of encoder and decoder inputs')
+tf.app.flags.DEFINE_integer('embedding_size', 200, 'Embedding dimensions of encoder and decoder inputs')
 
 tf.app.flags.DEFINE_float('learning_rate', 0.0001, 'Learning rate')
-tf.app.flags.DEFINE_integer('batch_size', 128, 'Batch size')
+tf.app.flags.DEFINE_integer('batch_size', 1, 'Batch size')
 tf.app.flags.DEFINE_integer('numEpochs', 30, 'Maximum # of training epochs')
 tf.app.flags.DEFINE_integer('steps_per_checkpoint', 100, 'Save model checkpoint every this iteration')
 tf.app.flags.DEFINE_string('model_dir', 'model/', 'Path to save model checkpoints')
 tf.app.flags.DEFINE_string('model_name', 'chatbot.ckpt', 'File name used for model checkpoints')
-tf.app.flags.DEFINE_string('data_path', 'gensim2file.pkl', 'Filepath of intput data')
+tf.app.flags.DEFINE_string('data_path', 'data/gensim2file.pkl', 'Filepath of intput data')
 FLAGS = tf.app.flags.FLAGS
-
+checkpoint_path = os.path.join(FLAGS.model_dir, FLAGS.model_name)
 word2id, id2word, trainingSamples = loadDataset(FLAGS.data_path)
 
 with tf.Session() as sess:
-    model = Seq2SeqModel(FLAGS.rnn_size, FLAGS.num_layers, FLAGS.embedding_size, FLAGS.learning_rate, word2id,
-                         mode='train', use_attention=True, beam_search=False, beam_size=5, max_gradient_norm=5.0)
+    model = Seq2SeqModel(FLAGS.rnn_size, FLAGS.num_layers, FLAGS.embedding_size, FLAGS.learning_rate, word2id, mode='train', use_attention=True, beam_search=False, beam_size=5, max_gradient_norm=5.0, gpu_num='0')
     ckpt = tf.train.get_checkpoint_state(FLAGS.model_dir)
     if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
+        print('================================')
         print('Reloading model parameters..')
-        model.restore(sess, ckpt.model_checkpoint_path)
+        print('================================')
+        model.saver.restore(sess, ckpt.model_checkpoint_path)
     else:
+        print('================================')
         print('Created new model parameters..')
+        print('================================')
         sess.run(tf.global_variables_initializer())
     current_step = 0
     summary_writer = tf.summary.FileWriter(FLAGS.model_dir, graph=sess.graph)
@@ -36,11 +39,29 @@ with tf.Session() as sess:
         print("----- Epoch {}/{} -----".format(e + 1, FLAGS.numEpochs))
         batches = getBatches(trainingSamples, FLAGS.batch_size)
         for nextBatch in tqdm(batches, desc="Training"):
+            print('*************************************')
+            print(len(nextBatch.encoder_inputs))
             loss, summary = model.train(sess, nextBatch)
+            print(nextBatch)
+            print('*************************************')
             current_step += 1
             if current_step % FLAGS.steps_per_checkpoint == 0:
+                model.saver.save(sess, checkpoint_path, global_step=current_step)
                 perplexity = math.exp(float(loss)) if loss < 300 else float('inf')
                 tqdm.write("----- Step %d -- Loss %.2f -- Perplexity %.2f" % (current_step, loss, perplexity))
                 summary_writer.add_summary(summary, current_step)
-                checkpoint_path = os.path.join(FLAGS.model_dir, FLAGS.model_name)
-                model.saver.save(sess, checkpoint_path, global_step=current_step)
+                with tf.Session() as predic_sess:
+                    # create a Seq2SeqModel for decoding
+                    decodingModel = Seq2SeqModel(FLAGS.rnn_size, FLAGS.num_layers, FLAGS.embedding_size, FLAGS.learning_rate, word2id, mode='decode', use_attention=True, beam_search=False, beam_size=5, max_gradient_norm=5.0, gpu_num='1')
+                    print('================================')
+                    print('Decoding Mode:')
+                    print('Reloading model parameters..')
+                    print('================================')
+                    ckpt = tf.train.get_checkpoint_state(FLAGS.model_dir)
+                    decodingModel.saver.restore(predic_sess, ckpt.model_checkpoint_path)
+                    # decode sentences from training data
+                    print('======================================================')
+                    print(decodingModel.infer(predic_sess, nextBatch, id2word))
+                    print('======================================================')
+                    # # decode sentences from testing data
+                    # print(decode.infer(predic_sess, nextBatch))
